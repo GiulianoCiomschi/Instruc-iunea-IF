399685c805cb11aef173147bb5819574defc832c
Ciomschi Giuliano <70976122+GiulianoCiomschi@users.noreply.github.com>
2020-10-15 10:41:50 +0300

    Add files via upload

---
+++ Untitled-11.py
@@ -0,0 +1,16 @@
+n1=int(input("dati n1:"))
+n2=int(input("dati n2:"))
+n3=int(input("dati n3:"))
+if (n1%2==0):
+    print(n1,"par",sep=" ")
+else:
+    print(n1,"impar",sep=" ")
+if (n2%2==0):
+    print(n2,"par",sep=" ")
+else:
+    print(n2,"impar",sep=" ")
+if (n3%2==0):
+    print(n3,"par",sep=" ")
+else:
+     print(n3,"impar",sep=" ")
+
